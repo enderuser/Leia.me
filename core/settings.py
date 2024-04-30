f1c004248312e10b6a821ee1facc807428f6a405
Kender User <wender@tisistema.com>
2024-04-29 22:20:55 -0300

    app para lidar com os emprestimos

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuarios',
     'divulgar',
+    'emprestar',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('usuarios.urls')),
     path('divulgar/', include('divulgar.urls')),
+    path('/', include('emprestar.urls')),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
