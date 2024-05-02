f5101345409dae3ea80e021c13aac9eec0555a7e
Kender User <wender@tisistema.com>
2024-05-02 08:35:25 -0300

    Atualizado app de listagem geral dos livros disponiveis para emprestimo

--- emprestar/urls.py
+++ emprestar/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.EmprestarListView, name="DivulgarList"),
+    path('', views.EmprestarListView, name="EmprestarList"),
     
 ]
