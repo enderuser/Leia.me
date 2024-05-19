d88285bd70208b95edaf11290d16695230fb001c
Kender User <wender@tisistema.com>
2024-05-19 16:13:41 -0300

    app para emprestar os livros atualizado

--- emprestar/views.py
+++ emprestar/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def EmprestarListView(request):
     if request.method == "GET":
         return render(request, 'emprestar_list.html')
